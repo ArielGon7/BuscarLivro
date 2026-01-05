5b92c6596f1dffb1310b633bef384b2c0af8f0e7
ArielGon7 <arielgomessantos7@gmail.com>
2026-01-05 16:23:25 -0300

    Implementação da classe Livro

--- src/main/domain/Livro.java
+++ src/main/domain/Livro.java
@@ -1,4 +1,42 @@
 package main.domain;
 
 public class Livro {
+    private Integer id;
+    private String tittle;
+    private int chapters;
+
+    public Livro(Integer id, String tittle, int chapters) {
+        this.id = id;
+        this.tittle = tittle;
+        this.chapters = chapters;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "id=" + id +
+                ", tittle='" + tittle + '\'' +
+                ", chapters=" + chapters +
+                '}';
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getTittle() {
+        return tittle;
+    }
+
+    public int getChapters() {
+        return chapters;
+    }
+
+    public void setTittle(String tittle) {
+        this.tittle = tittle;
+    }
+
+    public void setChapters(int chapters) {
+        this.chapters = chapters;
+    }
 }
